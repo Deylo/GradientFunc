a87d4f446a0685f9f9e24106529d0cf7dfeac3a3
Deylo <Deylo@users.noreply.github.com>
2016-01-23 19:39:35 +0200

    restructured code and rename some function

--- js.js
+++ js.js
@@ -1,47 +1,48 @@
 /**
  * Created by dmitriy on 19.01.16.
  */
-function gradient(fColor,lColor,cl){
-    var elements=document.getElementsByClassName(cl);
-    for(var i = 0;i<elements.length;i++){
-        m(elements[i]);
+function gradient(fColor,lColor,cl) {
+    var elements = document.getElementsByClassName(cl);
+    for (var i = 0; i < elements.length; i++) {
+        generateHTML(elements[i]);
     }
 
-    function m(elem) {
+    function generateHTML(elem) {
         var letterArr = elem.innerHTML.split('');
         var buf = '';
-        var fColorArr;
-        var lColorArr;
-        var n = [];
         var i;
-        var letterColor = fColor;
-
-        fColorArr = splitColor(fColor);
-        lColorArr = splitColor(lColor);
-
-
-        for (i = 0; i < 3; i++) {
-            n[i] = (parseInt(lColorArr[i], 16) - parseInt(fColorArr[i], 16)) / letterArr.length;
-        }
-
+        var rgbArr=generateRgb(fColor,lColor,letterArr.length);
 
         for (i = 0; i < letterArr.length; i++) {
-            buf += '<span style="color:' + letterColor + '">' + letterArr[i] + '</span>';
-            letterColor = rgbColor(fColorArr, n, i);
+            buf += '<span style="color:' + rgbArr[i] + '">' + letterArr[i] + '</span>';
         }
-
         elem.innerHTML = buf;
     }
 
-    function splitColor(c){
+    function generateRgb(fC, lC, n) {
+        var rgb=[];
         var buf = [];
-        for(var i = 0;i<3;i++) buf[i] = c.slice(1+i*2,3+i*2);
-        return buf;
+        var avrColor=[];
+        var fColorArr;
+        var lColorArr;
+        var i;
+        fColorArr = splitToRgb(fC);
+        lColorArr = splitToRgb(lC);
+
+        for (i = 0; i < 3; i++) {
+            avrColor[i] = (lColorArr[i] - fColorArr[i])/n;
+        }
+
+        for(i = 0;i < n;i++){
+            for (var j = 0; j < 3; j++) buf[j] = Math.floor(fColorArr[j] + avrColor[j] * i);
+            rgb.push('rgb('+buf.join(',')+')');
+        }
+        return rgb;
     }
 
-    function rgbColor(a,b,c){
+    function splitToRgb(c) {
         var buf = [];
-        for(var i = 0;i<3;i++) buf[i] = Math.floor(parseInt(a[i],16)+b[i]*c);
-        return 'rgb('+buf.join(",")+')';
+        for (var i = 0; i < 3; i++) buf[i] = parseInt(c.slice(1 + i * 2, 3 + i * 2),16);
+        return buf;
     }
 }
